e07f3f2f60f0cbb44b60d56609208e3c8f1e31bc
Fabian Beringer <fabian.beringer@smartsteuer.de>
2025-12-01 23:18:32 +0100

    day 1 part 2

--- 2025/day_01.py
+++ 2025/day_01.py
@@ -1,3 +1,5 @@
+from math import floor
+
 from utils import read_file_as_lines
 
 
@@ -14,11 +16,27 @@ def part1(nums: tuple[int, ...]) -> int:
     return zero_count
 
 
+def part2(nums: tuple[int, ...]) -> int:
+    zero_count = 0
+
+    dial = 50
+    for num in nums:
+        zero_count += floor(abs(num) / 100)
+        extra_step = num % -100 if num < 0 else num % 100
+        if dial + extra_step < 0 or dial + extra_step > 99:
+            zero_count += 1
+
+        dial = (dial + num) % 100
+
+    return zero_count
+
+
 def main():
     raw_lines = read_file_as_lines("data/day_01.txt")
     nums = tuple(map(lambda x: int(x.replace("L", "-").replace("R", "+")), raw_lines))
 
     print(f"Part 1: {part1(nums)}")
+    print(f"Part 2: {part2(nums)}")
 
 
 if __name__ == "__main__":
