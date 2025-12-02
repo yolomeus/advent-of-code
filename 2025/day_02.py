0ab375eaf69b03702211c5eb2f97b1a2ebefc980
Fabian Beringer <fabian.beringer@smartsteuer.de>
2025-12-02 14:59:25 +0100

    day 2 part 2

--- 2025/day_02.py
+++ 2025/day_02.py
@@ -1,3 +1,5 @@
+from collections.abc import Callable
+
 from utils import read_file
 
 
@@ -8,9 +10,7 @@ def get_ranges(raw_text: str):
     ]
 
 
-def is_valid_id(id_: int) -> bool:
-    id_ = str(id_)
-
+def is_valid_part1(id_: str) -> bool:
     if len(id_) % 2 != 0:
         return True
 
@@ -19,11 +19,26 @@ def is_valid_id(id_: int) -> bool:
     return a != b
 
 
-def part1(ranges: list[range]) -> int:
+def is_valid_part2(id_: str) -> bool:
+    # for at repeating number we need to partition into at least two numbers
+    id_middle = len(id_) // 2
+    possible_lengths = tuple(range(1, id_middle + 1))
+
+    for length in possible_lengths:
+        all_sub_ids_equal = all(
+            id_[i : i + length] == id_[:length] for i in range(0, len(id_), length)
+        )
+        if all_sub_ids_equal:
+            return False
+
+    return True
+
+
+def sum_invalid_ids(ranges: list[range], is_valid: Callable[[str], bool]) -> int:
     invalid_id_sum = 0
     for range_ in ranges:
         for id_ in range_:
-            if not is_valid_id(id_):
+            if not is_valid(str(id_)):
                 invalid_id_sum += id_
 
     return invalid_id_sum
@@ -33,7 +48,8 @@ def main():
     raw_text = read_file("data/day_02.txt")
     ranges = get_ranges(raw_text)
 
-    print("Part 1:", part1(ranges))
+    print("Part 1:", sum_invalid_ids(ranges, is_valid_part1))
+    print("Part 2:", sum_invalid_ids(ranges, is_valid_part2))
 
 
 if __name__ == "__main__":
