6fef902e76cfb94413e54545bad1e02ae347312a
Fabian Beringer <fabian.beringer@smartsteuer.de>
2025-12-04 02:06:03 +0100

    generalize

--- 2025/day_03.py
+++ 2025/day_03.py
@@ -1,39 +1,33 @@
 from collections.abc import Callable
+from functools import partial
 
 from utils import read_file_as_lines
 
 
 def sum_joltages(
     battery_banks: tuple[tuple[int, ...], ...],
-    compute_joltage: Callable[[tuple[int, ...]], int],
+    compute_joltage_fn: Callable[[tuple[int, ...]], int],
 ) -> int:
     total_joltage = 0
     for line in battery_banks:
-        total_joltage += compute_joltage(line)
+        total_joltage += compute_joltage_fn(line)
 
     return total_joltage
 
 
-def compute_joltage_part1(battery_bank: tuple[int, ...]):
-    max_joltage = 0
-    for i in range(len(battery_bank) - 1):
-        for j in range(len(battery_bank) - 1, i, -1):
-            joltage = int(f"{battery_bank[i]}{battery_bank[j]}")
-            max_joltage = max(max_joltage, joltage)
-    return max_joltage
-
-
-def compute_joltage_part2(battery_bank: tuple[int, ...]):
+def compute_joltage(battery_bank: tuple[int, ...], n_batteries: int) -> int:
     max_joltage = battery_bank[:1]
     rest = battery_bank[1:]
     while rest:
         num, *rest = rest
         while (
-            max_joltage and num > max_joltage[-1] and len(max_joltage) + len(rest) >= 12
+            max_joltage
+            and num > max_joltage[-1]
+            and len(max_joltage) + len(rest) >= n_batteries
         ):
             max_joltage = max_joltage[:-1]
 
-        if len(max_joltage) < 12:
+        if len(max_joltage) < n_batteries:
             max_joltage = max_joltage + (num,)
 
     return int("".join(map(str, max_joltage)))
@@ -43,8 +37,14 @@ def main():
     lines = read_file_as_lines("data/day_03.txt")
     battery_banks = tuple(map(lambda x: tuple(map(int, x)), lines))
 
-    print("Part 1:", sum_joltages(battery_banks, compute_joltage_part1))
-    print("Part 2:", sum_joltages(battery_banks, compute_joltage_part2))
+    print(
+        "Part 1:",
+        sum_joltages(battery_banks, partial(compute_joltage, n_batteries=2)),
+    )
+    print(
+        "Part 2:",
+        sum_joltages(battery_banks, partial(compute_joltage, n_batteries=12)),
+    )
 
 
 if __name__ == "__main__":
