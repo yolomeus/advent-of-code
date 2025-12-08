495fa67e60b5ab707520661a8919804ad4cba569
Fabian Beringer <fabian.beringer@smartsteuer.de>
2025-12-08 22:03:42 +0100

    day 6 part 2

--- 2025/day_06.py
+++ 2025/day_06.py
@@ -4,9 +4,9 @@ from functools import reduce
 from utils import read_file_as_lines
 
 operations = {
-    '*': lambda a, b: a * b,
-    '+': lambda a, b: a + b,
-    '-': lambda a, b: a - b,
+    "*": lambda a, b: a * b,
+    "+": lambda a, b: a + b,
+    "-": lambda a, b: a - b,
 }
 
 
@@ -21,10 +21,48 @@ def part1(homework_lines: Iterable[list[str]]):
     return total
 
 
+def _rpad(line: list[str], length: int) -> list[str]:
+    return line + [" "] * (length - len(line))
+
+
+def part2(homework_lines_raw: tuple[str, ...]) -> int:
+    homework_lines = list(map(lambda x: list(x.replace("\n", " ")), homework_lines_raw))
+
+    # make sure all lines have the same length
+    max_len = max(len(line) for line in homework_lines)
+    *homework_lines, op_symbols = [
+        list(reversed(_rpad(line, max_len))) for line in homework_lines
+    ]
+
+    total = 0
+    current_operands = []
+    j = 0
+    while j < len(homework_lines[0]):
+        current_operand = ""
+        for i in range(len(homework_lines)):
+            current_symbol = homework_lines[i][j]
+            if current_symbol.isdigit():
+                current_operand += current_symbol
+
+        if current_operand:
+            current_operands.append(int(current_operand))
+
+        if op_symbols[j] != " ":
+            total += reduce(operations[op_symbols[j]], current_operands)
+            current_operands = []
+
+        j += 1
+
+    return total
+
+
 def main():
-    homework_lines = map(str.split, read_file_as_lines("data/day_06.txt"))
-    print("Part 1:", part1(homework_lines))
+    homework_lines_parsed = list(map(str.split, read_file_as_lines("data/day_06.txt")))
+    print("Part 1:", part1(homework_lines_parsed))
+
+    homework_lines_raw = read_file_as_lines("data/day_06.txt", strip_lines=False)
+    print("Part 2:", part2(homework_lines_raw))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,9 +1,14 @@
 from os import PathLike
 
 
-def read_file_as_lines(filepath: str | PathLike) -> tuple[str]:
+def read_file_as_lines(
+    filepath: str | PathLike, strip_lines: bool = True
+) -> tuple[str, ...]:
     with open(filepath, "r", encoding="utf-8") as fp:
-        return tuple(map(lambda x: x.strip(), fp.readlines()))
+        lines = fp.readlines()
+        if strip_lines:
+            return tuple(map(lambda x: x.strip(), lines))
+        return tuple(lines)
 
 
 def read_file_as_tuples(filepath: str | PathLike) -> tuple[tuple[str, ...], ...]:
